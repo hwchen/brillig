70253b89fca4eaf3f7c602fe35211018aefbe08d
Walther Chen <walther.chen@gmail.com>
2024-03-16 17:32:09 -0400

    bril utils

--- flake.nix
+++ flake.nix
@@ -31,13 +31,35 @@
 
         zls = zls-flake.packages.${system}.zls;
 
+        venvDir = "./.venv";
+
         lib = pkgs.lib;
         in {
         devShells.default = pkgs.mkShell {
+        inherit venvDir;
         nativeBuildInputs = [
         pkgs.zigpkgs.master-2024-03-16
         zls
+
+        # for brili, check ~/.deno/bin when removing.
+        # install brili by deno install brili.ts
+        pkgs.deno
+        # bril2json and bril2txt
+        # requires
+        # ```
+        # pip install flit
+        # cd <bril-txt dir>
+        # flit install --symlink
+        # ```
+        pkgs.python311
+        # Just using venv, install everything by pip for python.
+        # https://www.reddit.com/r/NixOS/comments/q71v0e/what_is_the_correct_way_to_setup_pip_with_nix_to/
+        pkgs.python311Packages.venvShellHook
         ];
+        # for brili executable
+        PATH = "/home/hwchen/.deno/bin:$PATH";
+
+        # If needed I can define a postShellHook here.
       };
     });
 }
