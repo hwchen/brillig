25861f205900f9e9135a49a485af82268e519d2e
Walther Chen <walther.chen@gmail.com>
2024-03-17 14:08:15 -0400

    move json repr to own file

---
+++ src/json.zig
@@ -0,0 +1,81 @@
+const std = @import("std");
+
+pub const Program = struct {
+    functions: []Function,
+};
+
+pub const Function = struct {
+    name: []const u8,
+    args: ?[]const []const u8 = null,
+    type: ?[]const u8 = null,
+    instrs: []Code,
+};
+
+pub const Code = union(enum) {
+    Instruction: Instruction,
+    Label: Label,
+
+    pub fn jsonParse(allocator: std.mem.Allocator, source: anytype, options: std.json.ParseOptions) !@This() {
+        return try jsonParseFromValue(allocator, try std.json.innerParse(std.json.Value, allocator, source, options), options);
+    }
+
+    pub fn jsonParseFromValue(allocator: std.mem.Allocator, source: std.json.Value, options: std.json.ParseOptions) !@This() {
+        switch (source) {
+            .object => |object| {
+                if (object.contains("label")) {
+                    const label = try std.json.innerParseFromValue(Label, allocator, source, options);
+                    return Code{ .Label = label };
+                } else {
+                    const instr = try std.json.innerParseFromValue(Instruction, allocator, source, options);
+                    return Code{ .Instruction = instr };
+                }
+            },
+            else => return error.UnexpectedToken,
+        }
+    }
+
+    pub fn jsonStringify(self: @This(), stream: anytype) !void {
+        switch (self) {
+            inline else => |value| try stream.write(value),
+        }
+    }
+};
+
+pub const Label = struct {
+    label: []const u8,
+};
+
+pub const Instruction = struct {
+    op: []const u8,
+    dest: ?[]const u8 = null,
+    type: ?[]const u8 = null,
+    args: ?[]const []const u8 = null,
+    funcs: ?[]const []const u8 = null,
+    labels: ?[]const []const u8 = null,
+    value: ?Value = null, // for Constant
+};
+
+pub const Value = union(enum) {
+    bool: bool,
+    int: i64,
+
+    pub fn jsonParse(allocator: std.mem.Allocator, source: anytype, options: std.json.ParseOptions) !@This() {
+        return try jsonParseFromValue(allocator, try std.json.innerParse(std.json.Value, allocator, source, options), options);
+    }
+
+    pub fn jsonParseFromValue(allocator: std.mem.Allocator, source: std.json.Value, options: std.json.ParseOptions) !@This() {
+        _ = allocator;
+        _ = options;
+        switch (source) {
+            .bool => |b| return Value{ .bool = b },
+            .integer => |i| return Value{ .int = i },
+            else => return error.UnexpectedToken,
+        }
+    }
+
+    pub fn jsonStringify(self: @This(), stream: anytype) !void {
+        switch (self) {
+            inline else => |value| try stream.write(value),
+        }
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 /// Note: for json parsing, missing fields need default values
 ///
 const std = @import("std");
+const json = @import("json.zig");
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -13,7 +14,7 @@ pub fn main() !void {
     var in_buf: [4096]u8 = undefined;
     const bytes_read = try r.readAll(&in_buf);
     const in_bytes = in_buf[0..bytes_read];
-    const in_json = try std.json.parseFromSliceLeaky(Program, alloc, in_bytes, .{});
+    const in_json = try std.json.parseFromSliceLeaky(json.Program, alloc, in_bytes, .{});
 
     const transformed_bril = in_json;
 
@@ -23,83 +24,3 @@ pub fn main() !void {
     try std.json.stringify(transformed_bril, .{ .emit_null_optional_fields = false }, w);
     try out_buf_wtr.flush();
 }
-
-const Program = struct {
-    functions: []Function,
-};
-
-const Function = struct {
-    name: []const u8,
-    args: ?[]const []const u8 = null,
-    type: ?[]const u8 = null,
-    instrs: []Code,
-};
-
-const Code = union(enum) {
-    Instruction: Instruction,
-    Label: Label,
-
-    pub fn jsonParse(allocator: std.mem.Allocator, source: anytype, options: std.json.ParseOptions) !@This() {
-        return try jsonParseFromValue(allocator, try std.json.innerParse(std.json.Value, allocator, source, options), options);
-    }
-
-    pub fn jsonParseFromValue(allocator: std.mem.Allocator, source: std.json.Value, options: std.json.ParseOptions) !@This() {
-        switch (source) {
-            .object => |object| {
-                if (object.contains("label")) {
-                    const label = try std.json.innerParseFromValue(Label, allocator, source, options);
-                    return Code{ .Label = label };
-                } else {
-                    const instr = try std.json.innerParseFromValue(Instruction, allocator, source, options);
-                    return Code{ .Instruction = instr };
-                }
-            },
-            else => return error.UnexpectedToken,
-        }
-    }
-
-    pub fn jsonStringify(self: @This(), stream: anytype) !void {
-        switch (self) {
-            inline else => |value| try stream.write(value),
-        }
-    }
-};
-
-const Label = struct {
-    label: []const u8,
-};
-
-const Instruction = struct {
-    op: []const u8,
-    dest: ?[]const u8 = null,
-    type: ?[]const u8 = null,
-    args: ?[]const []const u8 = null,
-    funcs: ?[]const []const u8 = null,
-    labels: ?[]const []const u8 = null,
-    value: ?Value = null, // for Constant
-};
-
-const Value = union(enum) {
-    bool: bool,
-    int: i64,
-
-    pub fn jsonParse(allocator: std.mem.Allocator, source: anytype, options: std.json.ParseOptions) !@This() {
-        return try jsonParseFromValue(allocator, try std.json.innerParse(std.json.Value, allocator, source, options), options);
-    }
-
-    pub fn jsonParseFromValue(allocator: std.mem.Allocator, source: std.json.Value, options: std.json.ParseOptions) !@This() {
-        _ = allocator;
-        _ = options;
-        switch (source) {
-            .bool => |b| return Value{ .bool = b },
-            .integer => |i| return Value{ .int = i },
-            else => return error.UnexpectedToken,
-        }
-    }
-
-    pub fn jsonStringify(self: @This(), stream: anytype) !void {
-        switch (self) {
-            inline else => |value| try stream.write(value),
-        }
-    }
-};
