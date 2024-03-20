5d6499723f634f335f2d563f47b5d990ffabcf3e
Walther Chen <walther.chen@gmail.com>
2024-03-20 13:34:44 -0400

    In cfg gen, iterate through all instructions
    instead of just looking at the last instruction.
    
    This won't have a functional impact until we can search for function ->
    label mapping.
    
    Also, I think this fixes a bug where to check the end of all blocks, was
    instead looking at length of the current block.

--- src/analysis.zig
+++ src/analysis.zig
@@ -59,25 +59,25 @@ pub const ControlFlowGraph = HashMap([]const []const u8);
 
 pub fn controlFlowGraph(block_map: BlockMap, alloc: Allocator) !ControlFlowGraph {
     var cfg = ControlFlowGraph{};
-    const labels = block_map.map.keys();
-    const blocks = block_map.map.values();
-    for (0..block_map.map.count()) |i| {
-        const label = labels[i];
-        const block = blocks[i];
-        const last_instr = block[block.len - 1];
-        const succ = switch (last_instr) {
-            .Label => unreachable,
-            // Note that we can assume that instr.labels only exists on control ops w/ labels (jmp, br)
-            .Instruction => |instr| if (instr.op == .ret) continue else instr.labels orelse blk: {
-                var out = ArrayList([]const u8).init(alloc);
-                if (i <= block.len) {
-                    try out.append(try std.fmt.allocPrint(alloc, "{s}", .{labels[i + 1]}));
-                }
-                break :blk try out.toOwnedSlice();
-            },
-        };
-
-        try cfg.map.put(alloc, label, succ);
+    const blk_labels = block_map.map.keys();
+    const blks = block_map.map.values();
+    for (0..block_map.map.count()) |blk_idx| {
+        const blk_label = blk_labels[blk_idx];
+        const blk = blks[blk_idx];
+        var succs = ArrayList([]const u8).init(alloc);
+        for (blk, 0..) |code, code_idx| {
+            switch (code) {
+                .Label => unreachable,
+                // Note that we can assume that instr.labels only exists on control ops w/ labels (jmp, br)
+                .Instruction => |instr| if (instr.op == .ret) continue else if (instr.labels) |instr_labels| {
+                    try succs.appendSlice(instr_labels);
+                } else if (code_idx >= blk.len - 1 and blk_idx <= blks.len) {
+                    // last instruction in block, and is not the last block
+                    try succs.append(try std.fmt.allocPrint(alloc, "{s}", .{blk_labels[blk_idx + 1]}));
+                },
+            }
+        }
+        try cfg.map.put(alloc, blk_label, try succs.toOwnedSlice());
     }
     return cfg;
 }
