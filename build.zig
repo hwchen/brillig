e1941a8d7b321fea90693293766c1901cdd53bf1
Walther Chen <walther.chen@gmail.com>
2024-03-26 13:21:30 -0400

    remove root.zig and library refs in build.zig

--- build.zig
+++ build.zig
@@ -17,21 +17,6 @@ pub fn build(b: *std.Build) void {
 
     const clap = b.dependency("clap", .{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "bril-zig",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/root.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    lib.root_module.addImport("clap", clap.module("clap"));
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
-
     const exe = b.addExecutable(.{
         .name = "bril-zig",
         .root_source_file = .{ .path = "src/main.zig" },
@@ -68,16 +53,6 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/root.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     const exe_unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
@@ -90,6 +65,5 @@ pub fn build(b: *std.Build) void {
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
