afe7d9e053d842d4640197e3aaf71516a1517ba7
Walther Chen <walther.chen@gmail.com>
2024-03-30 19:05:06 -0400

    add odin to flake.nix

--- flake.nix
+++ flake.nix
@@ -22,10 +22,49 @@
 
   outputs = { self, nixpkgs, flake-utils, zig-overlay, zls-flake }:
     flake-utils.lib.eachDefaultSystem (system: let
+      odin-overlay = self: super: {
+        odin = super.odin.overrideAttrs (old: rec {
+          version = "dev-2024-03";
+          src = super.fetchFromGitHub {
+            owner = "odin-lang";
+            repo = "Odin";
+            rev = "${version}";
+            sha256 = "sha256-oK5OcWAZy9NVH19oep6QU4d5qaiO0p+d9FvxDIrzFLU=";
+          };
+
+          nativeBuildInputs = with super; [ makeWrapper which ];
+
+          LLVM_CONFIG = "${super.llvmPackages_17.llvm.dev}/bin/llvm-config";
+          postPatch = ''
+            sed -i 's/^GIT_SHA=.*$/GIT_SHA=/' build_odin.sh
+            sed -i 's/LLVM-C/LLVM/' build_odin.sh
+            patchShebangs build_odin.sh
+          '';
+
+          installPhase = old.installPhase + "cp -r vendor $out/bin/vendor";
+        });
+      };
+
+      ols-overlay = self: super: {
+        ols = super.ols.overrideAttrs (old: rec {
+          version = "nightly-2024-03-11-207fe98";
+          src = super.fetchFromGitHub {
+            owner = "DanielGavin";
+            repo = "ols";
+            rev = "207fe98a46b28297755608904dbf08d06fc50970";
+            sha256 = "sha256-S9cvLf8M0J7Zuf35RmZYtxJ6J215RoECK70SQBR+Kxk=";
+          };
+
+          installPhase = old.installPhase;
+        });
+      };
+
       pkgs = import nixpkgs {
         inherit system;
         overlays = [
           (zig-overlay.overlays.default)
+          (odin-overlay)
+          (ols-overlay)
         ];
         };
 
@@ -40,6 +79,9 @@
         nativeBuildInputs = [
         pkgs.zigpkgs.master-2024-03-16
         zls
+
+        pkgs.odin
+        pkgs.ols
 
         # for brili, check ~/.deno/bin when removing.
         # install brili by deno install brili.ts
